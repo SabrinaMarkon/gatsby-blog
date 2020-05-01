84c2477b6ba561797cbb94423bb5829fffbccd76
Sabrina Markon <phpsitescripts@outlook.com>
2020-04-30 23:05:53 -0600

    Add fake props to header test.

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -2,10 +2,24 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Header from './header';
 
+const fakeProps = {
+    siteTitle: 'Gatsby Blog',
+    tagline: 'My Blog',
+    author: 'Sabrina Markon',
+    contacts: {
+        github: 'SabrinaMarkon',
+        linkedin: 'SabrinaMarkon'
+    }
+}
+
 describe("Header", () => {
     it("renders correctly", () => {
         const tree = renderer
-            .create(<Header />)
+            .create(<Header
+                siteTitle={fakeProps.siteTitle}
+                tagline={fakeProps.tagline}
+                author={fakeProps.author}
+                contacts={fakeProps.contacts} />)
             .toJSON();
         expect(tree).toMatchSnapshot();
     });
